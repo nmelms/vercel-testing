1c783fb9db53110c08000c7836dbb95171d1b290
Nick Melms <nick@craftedagency.com>
2025-03-09 15:35:10 -0400

    another test

--- app/api/edge/route.tsx
+++ app/api/edge/route.tsx
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-export const config = {
-  runtime: "edge",
-};
+export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
   return NextResponse.json({ message: "Hello from the edge" });
